e0ac801d8c0d6caf591e29010d465f8057134197
Syd Bailey <sydabailey@gmail.com>
2019-07-22 20:14:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
-function takeANumber(deliLine, customerName) {
-  deliLine.push(customerName);
+function takeANumber(deliLine) {
+  var customerNumber = (deliLine.length + 1)
+  deliLine.push(customerNumber);
 
   if (deliLine.length === 0) {
     return "The line is currently empty."
   } else {
-    return `Welcome, ${customerName}. You are number ${deliLine.length} in line.`;
+    return `Welcome. You are number ${deliLine.length}.`;
  }
 }
 
-function currentLine(deliLine) {
-  const deliLineString = deliLine
-    .map((customerName, index) => `${index + 1}. ${customerName}`)
-    .join(", ");
-  if (deliLine.length === 0) {
-      return "The line is currently empty."}
-    else {
-      return `The line is currently: ${deliLineString}`;
- }
+function currentLine(lineArray) {
+  var theList = "The line is currently";
+
+  if (lineArray.length === 0) {
+      return theList += " empty.";
+}  else {
+    theList += ": ";
+    for (var i = 0; i < lineArray.length ; i++) {
+      theList += (i < lineArray.length-1) ?
+              `${i+1}. ${lineArray[i]}, `:`${i+1}. ${lineArray[i]}`;
+    }
+    return theList;
+  }
 }
 
 function nowServing(deliLine) {
-  const nowServedCustomer = deliLine.shift();
+  var nowServedCustomer = deliLine.shift();
 
   if (!nowServedCustomer) {
     return "There is nobody waiting to be served!";
